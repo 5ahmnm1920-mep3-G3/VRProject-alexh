92852f434aaa9e1edc0f5f7de621dd76ad19d0ad
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2020-04-13 17:06:04 +0200

    add some new Materials for the nametags and the sockets, import TMPro and add text to the nametags, place all the lights and set them up correctly, add PuzzleManager script and set up all variables in inspector, add Earth 3D model with seperate continents and attach Throwable and ContentPickUp scripts to them, also add colliders to the models

---
+++ Assets/MyGame/Scripts/ContinentPickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinentPickUp : MonoBehaviour
+{
+    private Transform tf;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tf = GetComponent<Transform>();
+    }
+
+    public void OnPickUp()
+    {
+        tf.SetParent(null);
+    }
+
+    public void OnRelease()
+    {
+        rb.isKinematic = false;
+    }
+}
---
+++ Assets/MyGame/Scripts/PuzzleManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public bool nametagSolved = false;
+    public bool continentSolved = false;
+    public bool puzzleSolved = false;
+
+    public GameObject nametag;
+    public GameObject continent;
+    public Light[] lights;
+
+    public Color32 standardColor;
+    public Color32 solvedColor;
+
+    private void SolvePuzzle()
+    {
+        puzzleSolved = true;
+
+        foreach (Light light in lights)
+        {
+            light.color = solvedColor;
+        }
+    }
+
+    private void UnsolvePuzzle()
+    {
+        puzzleSolved = false;
+
+        foreach (Light light in lights)
+        {
+            light.color = standardColor;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == nametag.name)
+        {
+            nametagSolved = true;
+            Debug.Log("nametag true");
+
+            if (continentSolved == true)
+            {
+                SolvePuzzle();
+            }
+        } 
+
+        if (collision.gameObject.name == continent.name)
+        {
+            continentSolved = true;
+            Debug.Log("continent true");
+
+            if (nametagSolved == true)
+            {
+                SolvePuzzle();
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == nametag.name)
+        {
+            nametagSolved = false;
+            Debug.Log("nametag false");
+
+            UnsolvePuzzle();
+        }
+
+        if (collision.gameObject.name == continent.name)
+        {
+            continentSolved = false;
+            Debug.Log("continent false");
+
+            UnsolvePuzzle();
+        }
+    }
+}
